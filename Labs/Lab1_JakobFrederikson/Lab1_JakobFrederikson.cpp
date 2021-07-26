6d6914bbcd22980fd560297af81656fe6d5b065d
Jakob <63636667+worzell@users.noreply.github.com>
2021-07-26 21:48:48 +1200

    Update Lab1_JakobFrederikson.cpp

--- Labs/Lab1_JakobFrederikson/Lab1_JakobFrederikson.cpp
+++ Labs/Lab1_JakobFrederikson/Lab1_JakobFrederikson.cpp
@@ -1,49 +1,96 @@
+//
+// Programmer: Jakob Frederikson
+// Student ID: 200729667
+//
+// Bachelor of Software Engineering
+// CS105 Development Principles II
+// Lab 1
+// 
+// Details:
+//		- Create two classes, 'Yacht' and 'Location'
+//		- 'Yacht' includes the yacht's number and location
+//		- Location includes degrees, minutes, and direction
+//		- A member function [ getpos() ] of 'Yacht' should get the location
+//		  of the yacht from the user.
+// 
+//		- Another member function [ display() ] should report the yacht 
+//		  number and location with the correct latitude and longitude format.
+// 
+//		- The main() function creates three yachts, asks the user to input the
+//		  the location and then displays each yachts number and location.
+//
+
 #include <iostream>
 using namespace std;
+
 class Location
 {
 public:
-    int degrees;
-    float minutes;
-    char direction;
+	int latDegrees;
+	int longDegrees;
+	float latMinutes;
+	float longMinutes;
+	char latDirection;
+	char longDirection;
 };
+
 class Yacht
 {
 public:
-    int serialNum;
-    Yacht(int yachtNum) // initialize the serial number
-    {
+	int serialNumber;
+	Location yachtLoc;
 
-    }
-    void get_pos() // get the location from class Location
-    {
+public:
+	Yacht(int s = 0, Location defLocation = { 0, 0, 0.0, 0.0, '/', '/' })
+	{
+		serialNumber = s;
+		yachtLoc = defLocation;
+	}
 
-    }
-    void display() // display the latitude and longitude
-    {
+public:
+	// Get yacht information from the user and store into the member variables.
+	void getpos()
+	{
+		cout << "Input degrees between 0 and 180: ";
+		cin >> yachtLoc.latDegrees;
+		cout << "Input minutes between 0 and 60:  ";
+		cin >> yachtLoc.latMinutes;
+		cout << "Input ship direction(N/E/S/W):   ";
+		cin >> yachtLoc.latDirection;
+		cout << "Input degrees between 0 and 180: ";
+		cin >> yachtLoc.longDegrees;
+		cout << "Input minutes between 0 and 60:  ";
+		cin >> yachtLoc.longMinutes;
+		cout << "Input ship direction(N/E/S/W):   ";
+		cin >> yachtLoc.longDirection;
+	}
 
-    }
+	// Display each yachts location and serial number.
+	void display()
+	{
+		cout << "Serial Number: " << serialNumber << endl;
+		cout << "Latitude:      " << yachtLoc.latDegrees << "\xF8" << yachtLoc.latMinutes << "\"" << yachtLoc.latDirection << endl;
+		cout << "Longitude:     " << yachtLoc.longDegrees << "\xF8" << yachtLoc.longMinutes << "\"" << yachtLoc.longDirection << endl << endl;
+	}
 };
+
 int main()
 {
-    cout << "************OCEAN RACE 2021-22************\n\n";
-    for (int i = 0; i < 3; i++)
-    {      
-        cout << "**********************************";
-        cout << "ENTER LOCATION FOR SHIP NUMBER " << i + 1;
-        cout << "Longitude:";
-        cout << "Input degrees between 0 and 180: ";
-        cout << "Input minutes between 0 and 60:  ";
-        cout << "Input direction (N/E/S/W):       ";
-        cout << "Latitude:";
-        cout << "Input degrees between 0 and 180: ";
-        cout << "Input minutes between 0 and 60:  ";
-        cout << "Input direction (N/E/S/W):       ";
-    }
-    cout << "******************************************\n\n";
-    for (int i = 0; i < 3; i++)
-    {
-        cout << "Ship serial number: ";
-        cout << "Latitude:          Longitude: ";
-    }
+	Yacht yachtObj[3];
+
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "\n[ENTER DETAILS FOR SHIP NUMBER " << i + 1 << "]";
+		cout << "\n---------------------------------\n";
+
+		yachtObj[i].getpos();
+		yachtObj[i].serialNumber = i + 1;
+	}
+	
+	cout << "\n[ALL SHIP DETAILS]";
+	cout << "\n==============\n";
+	for (int i = 0; i < 3; i++) 
+		yachtObj[i].display();	
+
+	return 0;
 }
