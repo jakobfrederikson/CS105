56c8a8cb5c944308072c72e3b55bbd0419292a03
Jakob <63636667+worzell@users.noreply.github.com>
2021-08-22 15:19:25 +1200

    Update TemplatePractice.cpp

--- TemplatePractice/TemplatePractice.cpp
+++ TemplatePractice/TemplatePractice.cpp
@@ -18,12 +18,25 @@ struct Functions // Structure within a template to define multiple functions.
     }
 };
 
+template <class B>
+static B add_something(B a, B b)
+{
+    return a + b;
+}
+
+template <class One, class Two>
+static One add_two_types(One a, Two b)
+{
+    return a + b;
+}
+
 int main()
 {
     long a = 3425, b = 7456, c;
     double d = 3.444, e = 78.323, f;
     float g = 55.345, h = 23.155, i;
     string hello = "Hello ", world = "World!", helloworld;
+    string myname = "My name ", isjakob = "is Jakob.", firstname;
     
     // functions being called from the class even when there is no object of the class
     c = Functions<long>::add_num(a , b); 
@@ -31,10 +44,17 @@ int main()
     i = Functions<float>::add_num(g, h); 
     helloworld = Functions<string>::add_num(hello, world); // add_num even works with string, because the function is just adding to variables together
 
+    firstname = add_something(myname, isjakob); // you don't need <string> since both data types are the same. The compiler will figure out what type you wanna return.
+
+    // add two different types together.
+    long ans = add_two_types(a, g); // added int and long together. Returns an int
+
     cout << c << endl;
     cout << f << endl;
     cout << i << endl;
     cout << helloworld << endl;
+    cout << firstname << endl;
+    cout << ans << endl;
 
     return 0;
 }
